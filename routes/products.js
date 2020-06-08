92f9220bcb3e10f924335a0507d0a72afbc0832d
andres-foo <jvillanelo@gmail.com>
2020-06-07 22:40:01 -0400

    Define routes and requests

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
-// routes
+// get all products
 router.get('/', (req, res) => {
   res.send('Get all products')
 })
+// get one product
+router.get('/:id', (req, res) => {
+  res.send('Get one product')
+})  
+
+// create one product
+router.post('/', (req, res) => {
+  res.send('Create one product')
+})
+
+// patch one product
+router.patch('/:id', (req, res) => {
+  res.send('Patch one product')
+})
+
+// delete one product
+router.delete('/:id', (req, res) => {
+  res.send('Delete one product')
+})
 
 module.exports = router
-
